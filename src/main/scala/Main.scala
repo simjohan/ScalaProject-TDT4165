2197678a02299156c718ea551346cd28214e94b4
Simen Johansen <simenjohansen@dhcp-10-24-18-172.wlan.ntnu.no>
2016-11-08 12:59:17 +0100

    Fixed some minor errors. The last test still fails.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,5 +9,20 @@ object Main extends App {
     }
 
   // Write a few transaction examples using Threads
+  var account1 = new Account(2000, Bank.getUniqueId)
+  var account2 = new Account(1500, Bank.getUniqueId)
+
+  println("Account1 balance: " + account1.getBalanceAmount)
+  println("Account2 balance: " + account2.getBalanceAmount)
+
+  var t1 = thread(Bank.transaction(account1, account2, 500))
+  var t2 = thread(Bank.transaction(account2, account1, 1000))
+
+  println("Account1 balance: " + account1.getBalanceAmount)
+  println("Account2 balance: " + account2.getBalanceAmount)
+  println("Pausing main thread for 2 seconds")
+  Thread.sleep(2000)
+  println("Account1 balance: " + account1.getBalanceAmount)
+  println("Account2 balance: " + account2.getBalanceAmount)
 
 }
