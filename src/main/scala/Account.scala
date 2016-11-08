760ea22e8b413dbbc12d90797f0cc79b3daf874e
Simen Johansen <simenjohansen@dhcp-10-24-18-172.wlan.ntnu.no>
2016-11-08 12:36:36 +0100

    Initial setup

---
+++ src/main/scala/Account.scala
@@ -0,0 +1,31 @@
+import exceptions.{IllegalAmountException, NoSufficientFundsException}
+
+class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
+
+  private var balance = initialBalance
+
+  def withdraw(amount: Double): Unit = {
+    if(amount > balance) {
+      throw new NoSufficientFundsException("Insufficient funds.")
+    }
+    else if (amount < 0) {
+      throw new IllegalAmountException("Invalid amount (negative).")
+    }
+    balance =  balance - amount
+  }
+
+
+  def deposit(amount: Double): Unit = {
+    if(amount <= 0) {
+      throw new IllegalAmountException("You must deposit a positive amount.")
+    }
+    balance = balance + amount
+  }
+
+
+  def getBalanceAmount: Double = {
+    balance
+  }
+
+
+}
