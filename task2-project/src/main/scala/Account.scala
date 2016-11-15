c5ea445063928b5fa6ba37f61090db32340167ac
Espen Meidell <espen.meidell@gmail.com>
2016-11-15 12:59:10 +0100

    Added old code to Account

--- task2-project/src/main/scala/Account.scala
+++ task2-project/src/main/scala/Account.scala
@@ -7,9 +7,22 @@ class Account(val bank: Bank, initialBalance: Double) {
   val balance = new Balance(initialBalance)
   val uid = bank.generateAccountId
 
-  def withdraw(amount: Double): Unit = ??? // Like in part 1
-  def deposit(amount: Double): Unit = ??? // Like in part 1
-  def getBalanceAmount: Double = ??? // Like in part 1
+
+  def withdraw(amount: Double): Unit = this.synchronized  {
+    if (amount > balance.amount) throw new NoSufficientFundsException("Too poor!")
+    if (amount < 0) throw new IllegalAmountException("Invalid amount (negative).")
+    balance.amount -= amount
+  }
+
+  def deposit(amount: Double): Unit = this.synchronized {
+    if (amount <= 0) throw new IllegalAmountException("Must be positive")
+    balance.amount += amount
+  }
+
+  def getBalanceAmount: Double = {
+    balance.amount
+  }
+
 
   def transferTo(account: Account, amount: Double) = {
     bank addTransactionToQueue (this, account, amount)
